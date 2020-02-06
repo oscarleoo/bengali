84f0f3be6deec697632851c9a9a0fda6d3f553c1
oscarleoo <oscar@aiwizo.com>
2020-02-06 07:39:03 +0100

    more augmentation

--- generators.py
+++ generators.py
@@ -12,11 +12,11 @@ trainIds = pd.read_csv('data/train.csv')
 trainIds = trainIds.set_index('image_id', drop=True)
 
 augmentor = AA.Compose([
-    AA.ShiftScaleRotate(scale_limit=0.05, rotate_limit=5, shift_limit=0.05, p=0.8, border_mode=cv2.BORDER_CONSTANT, value=0),
-    # AA.OpticalDistortion(distort_limit=0.1, shift_limit=0.05, p=0.8, border_mode=cv2.BORDER_CONSTANT, value=0),
-    # AA.GridDistortion(num_steps=3, distort_limit=0.1, p=0.8, border_mode=cv2.BORDER_CONSTANT, value=0),
+    AA.ShiftScaleRotate(scale_limit=0.1, rotate_limit=10, shift_limit=0.1, p=0.8, border_mode=cv2.BORDER_CONSTANT, value=0),
+    AA.OpticalDistortion(distort_limit=0.1, shift_limit=0.1, p=0.8, border_mode=cv2.BORDER_CONSTANT, value=0),
+    AA.GridDistortion(num_steps=3, distort_limit=0.1, p=0.8, border_mode=cv2.BORDER_CONSTANT, value=0),
     # AA.Cutout(num_holes=4, max_h_size=16, max_w_size=16, p=0.5)
-    # AA.RandomContrast(limit=0.4, p=0.8)
+    AA.RandomContrast(limit=0.2, p=0.8)
 ], p=1)
 
 def plot_augmentations():
--- predictions.py
+++ predictions.py
@@ -4,13 +4,13 @@ from sklearn.metrics import recall_score
 
 from algorithms import get_b0_backbone, get_b1_backbone, connect_simple_head
 from generators import get_data_generators
-
+from keras.models import load_model
 
 
 train_generator, valid_generator = get_data_generators('split1', 64)
 
 backbone, backbone_output = get_b1_backbone()
-model = connect_simple_head(backbone, backbone_output)
+model = load_model('model.h5')
 
 model.save('results/b1_simple/model.h5')
 with open("results/b1_simpe/model.json", "w") as json_file:
@@ -18,12 +18,60 @@ with open("results/b1_simpe/model.json", "w") as json_file:
 
 model.load_weights('results/b1_simple/final_step_weights.h5')
 predictions = valid_generator.make_predictions(model)
-trainIds = pd.read_csv('data/train.csv').set_index('image_id')
-trainIds.head()
-
 predictions = predictions.sort_index()
+
+predictions
+
+trainIds = pd.read_csv('data/train.csv').set_index('image_id')
 validIds = trainIds[trainIds.index.isin(predictions.index)].sort_index()
+
+
 validIds.head(5)
+
+predictions.head()
+
+
+from sklearn.metrics import  multilabel_confusion_matrix
+
+def calculate_class_weights(y_true, y_pred, title, alpha):
+    MCM = multilabel_confusion_matrix(
+        y_true[title].values.astype(int),
+        y_pred[title].values.astype(int)
+    )
+    true_positives = MCM[:, 1, 1]
+    true_sum = true_positives + MCM[:, 1, 0]
+    class_recall = (true_positives / true_sum) + alpha
+    class_recall = 1 / class_recall
+    class_recall = class_recall / class_recall.sum()
+    class_index = [i for i in range(len(class_recall))]
+    return pd.DataFrame([class_index, class_recall], index=[title, '{}_weight'.format(title)]).T
+
+calculate_class_weights(validIds, predictions, 'vowel_diacritic', 0.1)
+
+true_sum
+
+1 / class_recall
+
+tp_sum
+true_sum
+pred_sum
+
+validIds['vowel_diacritic'].value_counts().sort_index()
+tp_sum / true_sum
+
+
+validIds['consonant_diacritic'].values.astype(int)
+for i in range()
+
+
+hmm.shape
+
+    tp_sum = MCM[:, 1, 1]
+    pred_sum = tp_sum + MCM[:, 0, 1]
+    true_sum = tp_sum + MCM[:, 1, 0]
+
+
+classification_report(validIds['vowel_diacritic'].values.astype(int), predictions['vowel_diacritic'].values.astype(int))
 
 
 scores = []
