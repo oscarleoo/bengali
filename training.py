08347a0dd40bc8defdd1850e48f14fd6b0763561
oscarleoo <oscar@aiwizo.com>
2020-03-07 19:47:56 +0100

    added focal loss

--- training.py
+++ training.py
@@ -12,6 +12,7 @@ from keras.callbacks import EarlyStopping, ReduceLROnPlateau, Callback, ModelChe
 
 from generators import get_data_generators
 from utils.weighted_recall import WeightedRecall
+from utils.binary_focal_loss import binary_focal_loss
 
 
 def swish(x, beta = 1):
@@ -68,7 +69,7 @@ def train_full_model(model, name, settings, retrain=False, recall=False):
         callbacks = [reduce_lr, early_stopping, model_checkpoint]
 
     print('Training Model...')
-    model.compile(optimizer=Adam(settings['learning_rate']), loss='binary_crossentropy')
+    model.compile(optimizer=Adam(settings['learning_rate']), loss=[binary_focal_loss(alpha=.25, gamma=2)])
     history = model.fit_generator(
         train_generator, steps_per_epoch=train_generator.__len__(), epochs=settings['epochs'],
         validation_data=valid_generator, validation_steps=valid_generator.__len__(),
